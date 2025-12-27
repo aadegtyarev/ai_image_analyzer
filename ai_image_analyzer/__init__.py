0dacd554ae7e6f50fde2a9c70980741d0c217be5
Alexander Degtyarev <a.degtyarev@wirenboard.com>
2025-12-27 04:48:26 +0400

    chore: restore export of resize_image_bytes for backward compatibility

--- ai_image_analyzer/__init__.py
+++ ai_image_analyzer/__init__.py
@@ -12,7 +12,7 @@ except Exception:
 
 from .config import Settings, load_config, load_config_from_env, read_prompt_file
 from .image_io import load_and_resize_image, resize_image_from_bytes
-from .image_processing import make_collage, build_collage_system_prompt
+from .image_processing import resize_image_bytes, make_collage, build_collage_system_prompt
 from .model_client import call_model_with_image, call_model_with_text_only, check_balance
 from .json_api import handle_json_request
 
@@ -23,6 +23,7 @@ __all__ = [
 	"read_prompt_file",
 	"load_and_resize_image",
 	"resize_image_from_bytes",
+	"resize_image_bytes",
 	"make_collage",
 	"build_collage_system_prompt",
 	"call_model_with_image",
